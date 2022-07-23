32bb2de53c2edc39b40f67366c565512d1cb3478
doozles411 <cindy.phelps01@gmail.com>
2022-07-22 22:27:24 -0700

    Create ability to add decimal points

--- scripts.js
+++ scripts.js
@@ -10,12 +10,12 @@ const deleteBtn = document.querySelector('.delete');
 const numberBtns = document.querySelectorAll('.number');
 const operatorBtns = document.querySelectorAll('.operator');
 const equalBtn = document.querySelector('.equal');
-//const decimalBtn = document.querySelector('.decimal');
-
+const decimalBtn = document.querySelector('.decimal');
 
 clearBtn.onclick = () => clearDisplay();
 deleteBtn.onclick = () => backspace();
 equalBtn.onclick = () => evaluateOperation();
+decimalBtn.onclick = () => decimalUpdate();
 numberBtns.forEach((button) => 
     button.addEventListener('click', () => numberUpdate(button.textContent))
 );
@@ -26,6 +26,9 @@ operatorBtns.forEach((button) =>
 function clearDisplay() {
     displayTop.textContent = '';
     displayBottom.textContent = '0';
+    firstNumber = '';
+    secondNumber = '';
+    currentOperator = null;
 }
 
 function backspace() {
@@ -43,6 +46,26 @@ function numberUpdate(number) {
     displayBottom.textContent += number;
 }
 
+function decimalUpdate() {
+    if (operatorEntered) {
+        displayBottom.textContent = '';
+        operatorEntered = false;
+    }
+    if (displayBottom.textContent === '') {
+        displayBottom.textContent = '0';
+    }
+    if (displayBottom.textContent.includes('.')) return
+    displayBottom.textContent += '.';
+}
+
+function appendPoint() {
+    if (shouldResetScreen) resetScreen()
+    if (currentOperationScreen.textContent === '')
+      currentOperationScreen.textContent = '0'
+    if (currentOperationScreen.textContent.includes('.')) return
+    currentOperationScreen.textContent += '.'
+  }
+
 function operatorUpdate(operator) {
     if (currentOperator !== null) evaluateOperation();
     currentOperator = operator;
@@ -53,10 +76,30 @@ function operatorUpdate(operator) {
 
 function evaluateOperation() {
     if (currentOperator === null || operatorEntered) return;
+    if (currentOperator === '÷' && displayBottom.textContent === '0') {
+        alert("Nice try, but we all know the universe breaks when you divide by 0!");
+        return
+    }
     secondNumber = displayBottom.textContent;
     displayBottom.textContent = evaluateEquation(currentOperator, firstNumber, secondNumber);
     displayTop.textContent = `${firstNumber} ${currentOperator} ${secondNumber} =`;
     currentOperator = null;
 }
 
-function evaluateEquation(operator, a, b) {}
+function evaluateEquation(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case 'x':
+            return Math.round((a * b) * 1000) / 1000;
+        case'÷':
+            if (b === 0) return null;
+            else return Math.round((a / b) * 1000) / 1000;
+        default:
+            return null;
+    }
+}
