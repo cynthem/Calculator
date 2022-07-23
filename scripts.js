6f95432b7ced1ecbf2b80f9342d5cc5bb8795275
doozles411 <cindy.phelps01@gmail.com>
2022-07-22 21:50:36 -0700

    Create operator function

--- scripts.js
+++ scripts.js
@@ -7,14 +7,15 @@ const displayTop = document.querySelector('.display-top');
 const displayBottom = document.querySelector('.display-bottom');
 const clearBtn = document.querySelector('.clear');
 const deleteBtn = document.querySelector('.delete');
-
 const numberBtns = document.querySelectorAll('.number');
 const operatorBtns = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.equal');
 //const decimalBtn = document.querySelector('.decimal');
-//const equalBtn = document.querySelector('.equal');
+
 
 clearBtn.onclick = () => clearDisplay();
 deleteBtn.onclick = () => backspace();
+equalBtn.onclick = () => evaluateOperation();
 numberBtns.forEach((button) => 
     button.addEventListener('click', () => numberUpdate(button.textContent))
 );
@@ -35,13 +36,27 @@ function backspace() {
 }
 
 function numberUpdate(number) {
-    if (displayBottom.textContent === '0') {
+    if (displayBottom.textContent === '0' || operatorEntered) {
         displayBottom.textContent = '';
+        operatorEntered = false;
     }
     displayBottom.textContent += number;
 }
 
 function operatorUpdate(operator) {
-    let bottomDisplay = displayBottom.textContent;
-    displayTop.textContent = `${bottomDisplay} ${operator}`;
+    if (currentOperator !== null) evaluateOperation();
+    currentOperator = operator;
+    firstNumber = displayBottom.textContent;
+    displayTop.textContent = `${firstNumber} ${currentOperator}`;
+    operatorEntered = true;
 }
+
+function evaluateOperation() {
+    if (currentOperator === null || operatorEntered) return;
+    secondNumber = displayBottom.textContent;
+    displayBottom.textContent = evaluateEquation(currentOperator, firstNumber, secondNumber);
+    displayTop.textContent = `${firstNumber} ${currentOperator} ${secondNumber} =`;
+    currentOperator = null;
+}
+
+function evaluateEquation(operator, a, b) {}
